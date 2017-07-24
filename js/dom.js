3b0a2b8b5ded4f40a6bee63c28816c68127734d2
Selbahc <selbahc@users.noreply.github.comm>
2017-07-24 11:38:01 +0200

    comments

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 ///////////////
-//DOM elements needed
+//DOM ELEMENTS
 const newGameElt = document.querySelector('#new-game');
 const diceImgElt = document.querySelector('#dice-img');
 const rollDiceElt = document.querySelector('#roll-dice');
@@ -10,3 +10,5 @@ const globalEltP1 = document.querySelector('.player-1 .global p');
 const globalEltP2 = document.querySelector('.player-2 .global p');
 const roundEltP1 = document.querySelector('.player-1 .round p');
 const roundEltP2 = document.querySelector('.player-2 .round p');
+//
+/////////////////////
